76af15d860e69ab4fe967714248a0032d96d3605
raphael <raphael.daubaire.a2sup@gmail.com>
2022-10-23 21:42:12 +0200

    change current_user role détection

--- app/controllers/editor/application_controller.rb
+++ app/controllers/editor/application_controller.rb
@@ -1,10 +1,10 @@
-module Admin
+module Editor
     class ApplicationController < ::ApplicationController
-        before_action :only_admin
+        before_action :only_editor
 
         private
-        def only_admin
-            if !user_signed_in? || current_user.role.right != 'admin'
+        def only_editor
+            if !user_signed_in? || current_user.role.right != 'admin' ||current_user.role.right != 'editor'
                 redirect_to root_path, danger: "veuillez vous connecter ou vous inscrire pour accéder à cette page"
             end
         end
