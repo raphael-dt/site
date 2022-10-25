ba18934345c0dc038cfd78689b381786acd93940
raphael <raphael.daubaire.a2sup@gmail.com>
2022-10-25 21:30:15 +0200

    Create controller for signalement for extern

--- app/controllers/signalements_controller.rb
+++ app/controllers/signalements_controller.rb
@@ -1,7 +1,17 @@
 class SignalementsController < ApplicationController
   def new
+    @signalements= Signalement.new
   end
 
   def create
+    @comment= Comment.find(params[:id])
+    @signalements=@comment.signalement.create(params, user: session[:auth]['id'])
+    if @signalement.save
+      redirect_to article_path(@comment.article), sucess: "Votre signalement a bien été enregistrer nous allons bientot traiter votre demande."
   end
+  private
+  def signalement_params
+    params.require(:signalement).permit(:cause, :comment)    
+  end
+  
 end
