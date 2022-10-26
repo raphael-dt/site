fd4fc3028de596c84c4b18e31541c1da9452df63
raphael <raphael.daubaire.a2sup@gmail.com>
2022-10-26 22:18:05 +0200

    resolve syntax problem into new signalements form

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   get '/signalements/new/:comment', to: "signalements#new", as: :new_signalements
-  post '/signalement', to: "signalements#create"
+  post '/signalements', to: "signalements#create"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   scope 'admin', module: 'admin', as: 'admin' do
