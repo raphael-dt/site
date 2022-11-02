ee504942b207e77a1aac1532d5e7817106b1d5ab
raphael <raphael.daubaire.a2sup@gmail.com>
2022-11-02 22:42:49 +0100

    add color to signalement admin index page

--- app/helpers/admin/signalements_helper.rb
+++ app/helpers/admin/signalements_helper.rb
@@ -2,5 +2,13 @@ module Admin::SignalementsHelper
     def resolve_route(comment, action)
         {'id'=>comment, 'action'=>action}        
     end
+    def color(signalement)
+        if !signalement.status.nil? && signalement.status
+            "table-sucess"
+        else
+            "table-warning"
+        end
+    end
+    
     
 end
